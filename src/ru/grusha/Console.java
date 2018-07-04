a874a72f609120a1486b1e393a47c8e00b4fb14e
nesyedobnaya <anastasia.pear97@gmail.com>
2018-07-04 10:51:47 +0500

    Добавление класса для создания отчетов

--- src/ru/grusha/Console.java
+++ src/ru/grusha/Console.java
@@ -1,17 +1,15 @@
 package ru.grusha;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.TreeSet;
 
 import ru.grusha.exeption.DocumentExistsExeption;
 import ru.grusha.factory.Generator;
-import ru.grusha.model.Document;
 import ru.grusha.staff.Person;
 import ru.grusha.storage.DocumentStorage;
 import ru.grusha.utils.DocumentType;
 import ru.grusha.utils.FactoryUtil;
 import ru.grusha.utils.JsonFileWriter;
+import ru.grusha.utils.ReportUtil;
 /**
  * 
  * @author nesyedobnaya
@@ -22,37 +20,20 @@ public class Console {
 	public static void main(String[] args) {
 
 		Generator generator = new Generator();
-		JsonFileWriter jsonWriter = new JsonFileWriter();
 		FactoryUtil.loadStaff();
 		
 		for (DocumentType type : DocumentType.values()) { 
 			try {
-				Document newDocument=generator.createDocument(type);
-				//System.out.println(newDocument.toString());//вывод полей документа в консоль для проверки
+				generator.createDocument(type);
 			}catch(DocumentExistsExeption ex){
 				System.out.println(ex.getMessage());	
 			}
-		}		
-        
-		//создание набора авторов
-		TreeSet<Person> setOfAuthors = new TreeSet<Person>();                 
-		for (Document document: DocumentStorage.data) {        	
-			setOfAuthors.add(document.getAuthor());        	
-		}
-                
-		//вывод отчета (перечень авторов и список созданных ими документов, отсортированных по дате регистрации и регистрационному номеру)
+		}		   
+	
+		TreeSet<Person> setOfAuthors = ReportUtil.authorsToSet(DocumentStorage.data);
+		ReportUtil.printReport(DocumentStorage.data);
 		for (Person authorFromSet: setOfAuthors) {
-			List<Document> documentsOfAuthor = new ArrayList<Document>();
-			System.out.println(" - "+authorFromSet.getFullName()); //вывод полного имени автора документа         	        	
-			for (Document documentFromStorage: DocumentStorage.data) {
-				if (authorFromSet.equals(documentFromStorage.getAuthor())) {
-					System.out.println("\t- "+documentFromStorage.print());
-					documentsOfAuthor.add(documentFromStorage);
-		        }        		
-			}
-		jsonWriter.reportToAFile(authorFromSet, documentsOfAuthor);
-		}         
+			new JsonFileWriter().reportToAFile(authorFromSet, ReportUtil.getDocumentsOfAuthor(DocumentStorage.data, authorFromSet));
+		}
 	}
 }
-	
-
