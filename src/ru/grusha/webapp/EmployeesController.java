2faef7440b4eb9968ab87e9d24f89aa13b142970
nesyedobnaya <anastasia.pear97@gmail.com>
2018-07-06 12:35:35 +0500

    Изменение адресов

--- src/ru/grusha/webapp/EmployeesController.java
+++ src/ru/grusha/webapp/EmployeesController.java
@@ -17,9 +17,8 @@ import ru.grusha.utils.JsonFileWriter;
 import ru.grusha.utils.ReportUtil;
 import ru.grusha.wrappers.Documents;
 
-@Path("/ecm")
+@Path("/employees")
 public class EmployeesController {
-	@Path("/employees")
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	public String employeesToJson(){
@@ -28,7 +27,7 @@ public class EmployeesController {
 		return peopleJson;
 	}
 	
-	@Path("/employees/{id}")
+	@Path("/{id}")
 	@GET
 	@Produces(MediaType.TEXT_XML)
 	public String documentsOfAuthorsToXML(@PathParam("id") int ID){
